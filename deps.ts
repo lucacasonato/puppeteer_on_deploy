fea0eab9f19265b15a568a99e234774557fc3f30
Guilherme Rodrigues <gui@futuredriven.dev>
2022-10-19 19:48:43 -0300

    Upgrade puppeteer

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
+export { default as puppeteer } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v7.3.0/mod.ts";
 export * from "https://esm.sh/preact@10.10.6";
 export { renderToString } from "https://esm.sh/preact-render-to-string@5.2.0?deps=preact@10.10.6";
